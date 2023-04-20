46cff96a5fee616d874557d318b56a879402dfb4
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2023-04-20 14:25:22 -0300

    Added chroma.js and applied it to calculate hex values

--- components/cube.tsx
+++ components/cube.tsx
@@ -1,9 +1,10 @@
 import { motion } from "framer-motion";
+import chroma from "chroma-js";
 
 interface CubeProps {
   size?: number;
-  color?: { lightFace: string; mediumFace: string; darkFace: string };
-  opacity?: string | number;
+  color?: string;
+  opacity?: number;
   rotation?: string;
   dragConstraints?: {
     left?: number;
@@ -16,50 +17,55 @@ interface CubeProps {
     bounceStiffness: number;
     bounceDamping: number;
   };
-  shadow?: string;
+  dropShadow?: boolean;
+}
+
+function generateCubeColors(color) {
+  const light = color;
+  const medium = chroma(color).darken(0.3).hex();
+  const dark = chroma(color).darken(0.8).hex();
+  return { light, medium, dark };
 }
 
 const Cube = ({
-  size,
-  color,
+  size = 100,
+  color = "#0D8E77",
   opacity,
   rotation,
-  dragConstraints,
-  dragElastic,
-  dragTransition,
-  shadow,
+  dragConstraints = { left: 15, right: 15, top: 15, bottom: 15 },
+  dragElastic = 0.5,
+  dragTransition = { bounceStiffness: 500, bounceDamping: 10 },
+  dropShadow = true,
 }: CubeProps) => {
+  const { light, medium, dark } = generateCubeColors(color);
+
   return (
     <motion.div
-      className={`${shadow ?? "drop-shadow-2xl"} w-fit h-fit`}
+      className={`${dropShadow && "drop-shadow-2xl"} w-fit h-fit`}
       drag
-      dragConstraints={
-        dragConstraints ?? { left: 15, right: 15, top: 15, bottom: 15 }
-      }
-      dragElastic={dragElastic ?? 0.5}
-      dragTransition={
-        dragTransition ?? { bounceStiffness: 500, bounceDamping: 10 }
-      }
+      dragConstraints={dragConstraints}
+      dragElastic={dragElastic}
+      dragTransition={dragTransition}
     >
       <svg
-        className={`opacity-[${opacity}] ${rotation}`}
+        style={{ opacity: opacity, transform: `rotate(${rotation}deg)` }}
         xmlns="http://www.w3.org/2000/svg"
         width={size}
-        height={size}
+        height={size - 14}
         fill="none"
-        viewBox="0 0 107 107"
+        viewBox="0 0 100 86"
       >
         <path
-          fill={color.lightFace ?? "#0D8E77"}
-          d="M53.557 54.645l-33.716-6.834 21.206-27.088 33.716 6.833-21.206 27.09z"
+          fill={dark}
+          d="M49.383 40.968l50.588-3.974-20.201 42.66-50.588 3.973 20.2-42.659z"
         ></path>
         <path
-          fill={color.darkFace ?? "#125354"}
-          d="M65.354 86.628L53.558 54.644l21.206-27.09L86.56 59.54 65.354 86.628z"
+          fill={medium}
+          d="M20.73 4.49l28.651 36.475-20.2 42.659L.528 47.149 20.73 4.49z"
         ></path>
         <path
-          fill={color.mediumFace ?? "#0F7065"}
-          d="M65.353 86.627l-33.717-6.834L19.84 47.81l33.717 6.834 11.796 31.984z"
+          fill={light}
+          d="M20.732 4.497L71.32.524l28.65 36.474-50.587 3.974L20.732 4.497z"
         ></path>
       </svg>
     </motion.div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,23 +7,19 @@ const IndexPage = () => {
       <motion.div
         className="w-fit h-fit absolute bottom-32 left-0"
         //had to put position values here instead of on the cube
-        initial={{ opacity: 0, x: "80vh", rotate: -180 }}
+        initial={{ opacity: 0, x: "80vh", rotate: 180 }}
         animate={{ opacity: 1, x: 0, rotate: 0 }}
         transition={{ duration: 2.5, ease: "easeInOut", delay: 1.3 }}
       >
         <Cube
-          size={307}
-          color={{
-            lightFace: "#0D8E77",
-            mediumFace: "#0F7065",
-            darkFace: "#125354",
-          }}
-          opacity={0.1}
-          rotation="rotate-180"
+          size={200}
+          color={"#0D8E77"}
+          opacity={0.6}
+          // rotation="150"
           dragConstraints={{ left: 15, right: 15, top: 15, bottom: 15 }}
           dragElastic={0.5}
           dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }}
-          shadow="drop-shadow-2xl"
+          dropShadow={true}
         ></Cube>
       </motion.div>
     </div>
