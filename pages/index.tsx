1101508fce482b0d48e795c6f4f237b334c04672
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2023-04-20 16:32:11 -0300

    deleted comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const IndexPage = () => {
   const [color, setColor] = useState("#0D8E77");
 
   return (
-    <div className="relative flex justify-center h-screen w-screen">
+    <div className="relative flex flex-col justify-center w-screen">
       <div className="flex gap-5 mt-10 ml-10">
         <button
           className="block w-10 h-4 p-2 text-sm rounded-2xl bg-[#e2c2f6]"
@@ -22,8 +22,7 @@ const IndexPage = () => {
         ></button>
       </div>
       <motion.div
-        className="w-fit h-fit absolute bottom-32 left-32"
-        //had to put position values here instead of on the cube
+        className="w-fit h-fit absolute top-32 left-32"
         initial={{ opacity: 0, x: "150vh", rotate: 180 }}
         animate={{ opacity: 1, x: 0, rotate: 0 }}
         transition={{ duration: 3, ease: "easeInOut", delay: 1.3 }}
@@ -32,7 +31,6 @@ const IndexPage = () => {
           size={200}
           color={color}
           opacity={0.6}
-          // rotation="150"
           dragConstraints={{ left: 15, right: 15, top: 15, bottom: 15 }}
           dragElastic={0.5}
           dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }}
